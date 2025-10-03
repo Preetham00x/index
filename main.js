e1a6dc1618ae209acda2ada5e3b9677f17c9dff9
Preetham P <preetham2006@outlook.com>
2025-10-03 23:37:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
-
-//arrays
-// //-const name=['preetham','technologia','niggers'];
-////name[3]='brownie'
-//console.log(Array.isArray('negro'))
-//name.push('liar')
-//name.unshift('elephant')
-//name.pop()
-//console.log(name.indexOf('brownie'))
-//console.log(name)
-
-//object literals
-//const guy={
-   // name:'preetham',
-//    age:18,
-  //  hobbies:['eat','sleep','binge'],
-   // address:{
-     //   street:'thota',
-       // pincode:500018,
-   // }
-//}
-//const {name,age}=guy;
-//console.log(guy)
-
-//for(let i=0;i<=10;i++){
-//   console.log(i);
-//}
 function Person(name,dob){
    this.name=name
    this.dob=new Date(dob)
